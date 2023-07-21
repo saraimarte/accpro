7d8e241d75588241cf00e42ae3e9eca908c5cdcc
Sarai Marte <smartepujols@gmail.com>
2023-07-21 17:29:45 -0400

    g

--- src/scripts/drag.js
+++ src/scripts/drag.js
@@ -1,5 +1,11 @@
 import dragula from 'dragula';
+var scrollable = true;
 
+var listener = function(e) {
+    if (! scrollable) {
+        e.preventDefault();
+    }
+}
 document.addEventListener('touchmove', function() { e.preventDefault(); }, { passive:false });
 
 function init() {
@@ -11,12 +17,16 @@ function init() {
 
     dragula(dropcontainers).on("drag", function(el){
         el.classList.remove("box1", "box2", "dragparent");
+        scrollable = false;
+
     })
     dragula(dropcontainers).on("drop", function(el, target, source) {
       // check if the target container is dragparent or dragparent2
       var containerClass = target.id;
       // add the new class to the dropped element
       el.classList.add(containerClass);
+      scrollable = true;
+
     })
   }
 
