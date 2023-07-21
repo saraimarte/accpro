8d84da049bf1a360999152b0282cc0cac390fa60
Sarai Marte <smartepujols@gmail.com>
2023-07-21 18:07:11 -0400

    g

--- src/scripts/drag.js
+++ src/scripts/drag.js
@@ -1,16 +1,22 @@
 import dragula from 'dragula';
 
+
 function init() {
+    var dropcontainers = [
+      document.querySelector("#dragparent"),
+      document.querySelector("#box1"), 
+      document.querySelector("#box2")
+    ];
+
     dragula(dropcontainers).on("drag", function(el){
         el.classList.remove("box1", "box2", "dragparent");
     })
     dragula(dropcontainers).on("drop", function(el, target, source) {
       // check if the target container is dragparent or dragparent2
-      var containerClass = target.id;
-      // add the new class to the dropped element
       el.classList.add(containerClass);
     })
   }
+
 window.onload = function (){
   init();
   console.log("THE DRAG IS WORKINGGG");
