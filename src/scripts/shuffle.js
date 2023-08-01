4a0b0a9b162dca2fc635f7c98b2b53739098bd92
Sarai Marte <smartepujols@gmail.com>
2023-08-01 10:54:47 -0400

    added new activities

--- src/scripts/shuffle.js
+++ src/scripts/shuffle.js
@@ -13,8 +13,21 @@ var activities = [
     "Interviewing job applicants",
     "Investing in stocks",
     "Attending an industry conference",
-  ]
-var answers = ['Yes', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'No', 'Yes', 'No', 'Yes', 'No']
+    "Selling handmade bracelets to a retailer for cash",    
+    "Paying a vendor $1,000 for printing marketing materials",
+    "Discussing paying a creditor",
+    "Collaborating with team members on a project",
+    "Signing a contract",  
+    "Paying Rent", 
+    "Selling a delivery truck",
+    "Collecting $2,000 payment from a customer who made a purchase on credit",
+    "Making a digital art piece for a customer" ,
+    "Designing a new company logo",
+    "Telling a creditor that you will pay them tomorrow"
+]
+var answers = ['Yes', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 
+'No', 'No', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'No', 
+'No', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'No', 'No']
 
 export function shuffle(arr1, arr2) {
     if (arr1.length !== arr2.length){
